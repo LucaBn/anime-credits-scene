bd1c102d7c080833dcb6a1bb562e74b8ad17ea2f
LucaBn <luca.bonotti@virgilio.it>
2023-02-12 23:00:00 +0100

    feat: :sparkles: add configuration script

--- index.js
+++ index.js
@@ -1,10 +1,41 @@
-"use strict";
-exports.__esModule = true;
-exports.animeCreditsScene = void 0;
-exports.animeCreditsScene = {
-    start: function () {
-        console.log("Started!");
-        return true;
+var animeCreditsScene = {
+    configData: {
+        zIndex: 9999,
+        bgImage: "assets/img/default.jpg",
+        bgSong: "assets/audio/default.mp3",
+        nameList: []
+    },
+    getConfigJSON: function () {
+        var configJSON = fetch("assets/anime-credits-scene-data.json")
+            .then(function (response) { return response.json(); })
+            .then(function (configData) { return configData; })["catch"](function (error) { return console.log(error); });
+        return configJSON;
+    },
+    setConfigData: function (_a) {
+        var configData = _a.configData;
+        Object.entries(configData)
+            .filter(function (configDataProperty) { return configDataProperty[1]; })
+            .forEach(function (configDataProperty) {
+            animeCreditsScene.configData[configDataProperty[0]] =
+                configDataProperty[1];
+        });
+    },
+    handleAudioData: function () {
+        fetch(animeCreditsScene.configData.bgSong)
+            .then(function (audioObject) { return audioObject.arrayBuffer(); })
+            .then(function (audioObject) {
+            var audioContext = new window.AudioContext();
+            audioContext.decodeAudioData(audioObject, function (buffer) {
+                console.log(buffer.duration);
+            });
+        });
+    },
+    run: function () {
+        var runPipe = Promise.resolve();
+        runPipe
+            .then(function () { return animeCreditsScene.getConfigJSON(); })
+            .then(function (configData) { return animeCreditsScene.setConfigData({ configData: configData }); })
+            .then(function () { return animeCreditsScene.handleAudioData(); });
     }
 };
-exports.animeCreditsScene.start();
+animeCreditsScene.run();
--- index.ts
+++ index.ts
@@ -1,8 +1,74 @@
-export const animeCreditsScene = {
-  start: (): boolean => {
-    console.log("Started!");
-    return true;
+// Types
+type NameListType = {
+  title: string | null;
+  subtitle: string | null;
+  nameList: string[];
+};
+
+type ConfigDataType = {
+  zIndex: number;
+  bgImage: string;
+  bgSong: string;
+  nameList: NameListType[];
+};
+
+// Interfaces
+interface ISetConfigData {
+  configData: ConfigDataType;
+}
+
+// Logic
+const animeCreditsScene = {
+  configData: <ConfigDataType>{
+    zIndex: 9999,
+    bgImage: "assets/img/default.jpg",
+    bgSong: "assets/audio/default.mp3",
+    nameList: [],
+  },
+
+  getConfigJSON: (): Promise<ConfigDataType> => {
+    const configJSON = fetch("assets/anime-credits-scene-data.json")
+      .then((response) => response.json())
+      .then((configData) => configData)
+      .catch((error) => console.log(error));
+
+    return configJSON;
+  },
+
+  setConfigData: ({ configData }: ISetConfigData): void => {
+    Object.entries(configData)
+      .filter((configDataProperty) => configDataProperty[1])
+      .forEach((configDataProperty) => {
+        animeCreditsScene.configData[configDataProperty[0]] =
+          configDataProperty[1];
+      });
+  },
+
+  handleAudioData: (): void => {
+    fetch(animeCreditsScene.configData.bgSong)
+      .then((audioObject) => audioObject.arrayBuffer())
+      .then((audioObject) => {
+        const audioContext = new window.AudioContext();
+        audioContext.decodeAudioData(audioObject, function (buffer) {
+          console.log(buffer.duration);
+        });
+        // TODO: play audio
+        // const audio = document.getElementById("audio") as HTMLAudioElement;
+        // audio.src = animeCreditsScene.configData.bgSong;
+        // audio.load();
+        // audio.play();
+      });
+  },
+
+  run: (): void => {
+    const runPipe = Promise.resolve();
+
+    runPipe
+      .then(() => animeCreditsScene.getConfigJSON())
+      .then((configData) => animeCreditsScene.setConfigData({ configData }))
+      .then(() => animeCreditsScene.handleAudioData());
   },
 };
 
-animeCreditsScene.start();
+// Run it!
+animeCreditsScene.run();
